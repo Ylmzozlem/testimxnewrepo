6d1c95a7f2c6935c47ab8448775c7786d31edcb3
Ozlem <ylmzozlem.3461@gmail.com>
2025-11-13 21:45:32 +0300

    Q3

--- src/main/java/GuessOutputQuestions/Q3.java
+++ src/main/java/GuessOutputQuestions/Q3.java
@@ -12,6 +12,9 @@ public class Q3 {
     public static void main(String[] args) {
         byte a=10;
         double b=  15;
+        byte a1=10;
+        double b2=  15;
+
 
         // byte<short<int<long<float<double
 
