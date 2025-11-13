238595ade3e12a09e62ab14c6b4ffe63d33d2151
Ozlem <ylmzozlem.3461@gmail.com>
2025-11-13 15:31:11 +0300

    try back

--- src/main/java/Dene.java
+++ src/main/java/Dene.java
@@ -1,5 +1,14 @@
 public class Dene {
     public static void main(String[] x) {
+        // sum metodundan dönen değeri yazdırıyoruz
+        int sonuc = Dene.sum(3, 5);
+        System.out.println("Toplam: " + sonuc);
 
+        // Veya direkt olarak metodu println içinde çağırabiliriz:
+        // System.out.println("Toplam: " + Dene.sum(3, 5));
+    }
+
+    public static int sum(int a,int b){
+        return a + b;
     }
 }
