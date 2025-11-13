e4dd9a3eb0bc23d82e17ddca6dc2fe366376d5cb
Yusuf Celal <yusufcelal34@gmail.com>
2025-11-13 22:06:01 +0300

    dd

--- src/main/java/Question07/LargestElement.java
+++ src/main/java/Question07/LargestElement.java
@@ -20,6 +20,15 @@ public class LargestElement {
         return largest;
     }
 
+    public static int yusuftest(int[] arr) {
+        int largest = arr[0];
+        for (int num : arr) {
+            if (num > largest) {
+                largest = num;
+            }
+        }
+        return largest;
+    }
     public static void main(String[] args) {
         int[] numbers = {10, 5, 8, 20, 15};
         int result = findLargest(numbers);
